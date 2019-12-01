eb93572bbe805cc0cdc0dd707973cd5a125c52c0
eizaburo <eizaburo.tamaki@gmail.com>
2019-12-01 14:20:12 +0900

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Nav, Navbar, NavbarBrand, NavbarToggler, Collapse, NavItem, NavLink, Jumbotron } from 'reactstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const styles = {
+    jumbotron: {
+        background: "url('http://www.eizaburo.com/img/top-img-3.jpg') center center",
+        backgroundSize: "cover",
+        color: "#fff",
+        borderRadius: 0,
+    }
+}
+
+
+class App extends React.Component {
+
+    state = {
+        isOpen: false,
+    }
+
+    toggle = () => {
+        this.setState({ isOpen: !this.state.isOpen });
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+
+                <Navbar color="light" light expand="sm">
+                    <NavbarBrand href="/">Logo</NavbarBrand>
+                    <NavbarToggler onClick={this.toggle} />
+                    <Collapse isOpen={this.state.isOpen} navbar>
+                        <Nav className="mr-auto" navbar>
+                            <NavItem><NavLink href="#">Home</NavLink></NavItem>
+                            <NavItem><NavLink href="#">Service</NavLink></NavItem>
+                            <NavItem><NavLink href="#">About</NavLink></NavItem>
+                        </Nav>
+                    </Collapse>
+                </Navbar>
+
+                <Jumbotron style={styles.jumbotron}>
+                    <h1>未来を作る会社です。</h1>
+                    <p>ITの力であなたのビジネスの課題を解決します。</p>
+                </Jumbotron>
+
+            </React.Fragment>
+        );
+    }
 }
 
 export default App;
